bd61bcee692459ae8f12bffc25df48767b586c02
yohannes4321 <alemuyohannes960@gmail.com>
2024-11-21 02:25:29 +0300

    eculdian

---
+++ Types_Similarity_search/Content_Based_Similarity.py
@@ -0,0 +1,25 @@
+Content-based filtering
+
+uses item features to recommend items simmilar to what the User
+likes,based from the previous actions of the User.
+
+This feature can be converted into numercal representation 
+
+
+ 1 TF-IDF(Term Frequency Inverse Document Frequency)
+how important a word is in a document
+TERM FREQUENCY:
+how frequent a term appear in frequancy which means higher frequancy means 
+higher importance
+
+inverse Doucment FREQUENCY:
+How important a term in entire corpus words that appear in many documents 
+are lower importance 
+words that is found is fewer documents are more important 
+
+
+Word Embeddings
+
+
+
+
